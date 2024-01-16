d04f40020481557a044856839d1ced1d85913c54
citizenfish <dave@nautoguide.com>
2024-01-16 09:13:26 +0000

    Proper app name

--- opennames/apps.py
+++ opennames/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GeocoderConfig(AppConfig):
+class OpennamesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'geocoder'
+    name = 'opennames'
